6c2a52e2af7d061b5c500dcdabdcffda3bb971ea
Joerg Wallmersperger <94985188+JoergWall@users.noreply.github.com>
2021-12-16 11:15:58 +0100

    Add files via upload

---
+++ bin/run_smartdoorF455.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# run this script with sudo like this
+# sudo run_smartdoorF455.sh
+# script decouples smartdoorF455 from shell and logs errors to logfile
+# creates ~/log directory in home directory, if not there already
+logfile=$(date +%Y%m%d\_%H%M%S)\_smartdoorF455.log
+USER_HOME=$(eval echo ~${SUDO_USER})
+logdir=$USER_HOME/log
+progdir=$USER_HOME/smartdoorF455/bin
+prog=smartdoorF455
+# check if run by sudo
+if [ "$EUID" -ne 0 ]
+then 
+    echo "Please run with sudo or as root"
+    exit
+fi
+# check for log directory
+if [ ! -d $logdir ] 
+then
+    mkdir $logdir
+    chown $SUDO_USER.$SUDO_GROUP $logdir
+    echo "$logdir created"
+fi
+# check for executable program file
+if [ ! -x $progdir/$prog ]
+then
+    echo "please first build $prog"
+    echo "check README.md"
+    exit
+fi
+# check if running already
+if pgrep -x $prog
+then 
+    echo "$prog already running"
+    exit
+fi
+# run $prog with nohup to decouple from shell
+nohup $progdir/$prog </dev/null > $logdir/$logfile 2>&1 &
+echo "$prog started successfully"
+echo "watch $logdir/$logfile for errors"
