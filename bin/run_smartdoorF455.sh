de07d703029b5b7c096e993b1542214b173c7b6b
Joerg Wallmersperger <joerg.wall@gmail.com>
2025-10-20 14:05:58 +0200

    ** C++ only release with config.toml config file support

--- bin/run_smartdoorF455.sh
+++ bin/run_smartdoorF455.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-# run this script with sudo like this
-# sudo run_smartdoorF455.sh
+# run this script like this
+# ./run_smartdoorF455.sh
 # script decouples smartdoorF455 from shell and logs errors to logfile
 # creates ~/log directory in home directory, if not there already
 logfile=$(date +%Y%m%d\_%H%M%S)\_smartdoorF455.log
-USER_HOME=$(eval echo ~${SUDO_USER})
+USER_HOME=$(eval echo ~${USER}) # not SUDO_USER, no SUDO required with WiringPi
 logdir=$USER_HOME/log
 progdir=$USER_HOME/smartdoorF455/bin
 prog=smartdoorF455
-# check if run by sudo
-if [ "$EUID" -ne 0 ]
-then 
-    echo "Please run with sudo or as root"
-    exit
-fi
+# cd to $progdir, as config file is located in relative path ../src
+cd $progdir
 # check for log directory
 if [ ! -d $logdir ] 
 then
@@ -29,10 +25,22 @@ then
     exit
 fi
 # check if running already
-if pgrep -x $prog
-then 
-    echo "$prog already running"
+process_id=$(pgrep -x $prog)
+if [ -z "$process_id" ]; then
+    echo "Process $prog not running."
+else
+    echo "$prog already running with process id $process_id"
     exit
+fi
+# check, if nice capability has been set to executable program
+# if not, try to set the capability with sudo setcap command
+nice_capability=$(setcap -v 'cap_sys_nice=eip' $progdir/$prog)
+nice_OK=${nice_capability: -2} # get last two characters of string
+echo "nice: $nice_OK"
+if [ "$nice_OK" != "OK" ]; then
+    echo "trying to set nice capability with this command:"
+    echo "sudo setcap 'cap_sys_nice=eip' $progdir/$prog"
+    sudo setcap 'cap_sys_nice=eip' $progdir/$prog
 fi
 # run $prog with nohup to decouple from shell
 nohup $progdir/$prog </dev/null > $logdir/$logfile 2>&1 &
@@ -41,4 +49,4 @@ echo "$prog started successfully with PID $PROG_PID"
 echo "watch $logdir/$logfile for errors"
 # obtain process id to set CPU affinity to CPU #3
 # assuming that isolcpus=3 as been added to /boot/cmdline.txt
-taskset -cp 3 $PROG_PID
+sudo taskset -cp 3 $PROG_PID
